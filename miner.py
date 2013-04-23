459ee8a79a007649aec8d42e1b3cf142c87d1afe
SoulByte <M8R-v5k0gx@mailinator.com>
2013-04-23 12:12:52 +0300

    Update System
    Users are now informed if they are using outdated miners.

--- miner.py
+++ miner.py
@@ -11,9 +11,13 @@ class BlooMiner:
         self.ip = "bloocoin.zapto.org"
         self.port = 3122
         self.addr = sys.argv[2]
+        self.type = "miner"
+        self.ver = "1"
+        self.update()
     def main(self):
+        print "Mining thread started."
         while True:
-
+            
             sha512 = hashlib.sha512         
             
             self.continue_mining = True
@@ -31,7 +35,7 @@ class BlooMiner:
             #hashes = 0
             #khps = time.time()
             
-            if difficulty % 2:
+            if difficulty % 2: # hexdigest() version - slow
                 while self.continue_mining:
                     if sha512(start_string + str(num)).hexdigest()[0:difficulty] == null:
                         print "Possible Solution: " + start_string + str(num)
@@ -44,7 +48,7 @@ class BlooMiner:
                         #hashes = 0
                         #khps = time.time()
             else:
-                difficulty /= 2
+                difficulty /= 2 # digest() version - fast
                 null = chr(0) * difficulty
                 while self.continue_mining:
                     if sha512(start_string + str(num)).digest()[0:difficulty] == null:
@@ -70,13 +74,15 @@ class BlooMiner:
 
     def get_coin(self):
         s = socket.socket()
-        try:
-            s.connect((self.ip, self.port))
-        except:
-            s.close()
-            time.sleep(10)
-            self.get_coin()
-            
+        while True:
+            try:
+                s.connect((self.ip, self.port))
+                break
+            except:
+                print "Can't connect to server. Trying again in 10 seconds."
+                s.close()
+                time.sleep(10)
+                continue
         s.send(json.dumps({"cmd":"get_coin"}))
         data = json.loads(s.recv(1024))
         s.close()
@@ -96,10 +102,42 @@ class BlooMiner:
             print "Winning solution found!"
         else:
             print "Not a winning solution"
+        return
+
+    def update(self):
+        s = socket.socket()
+        s.settimeout(3)
+        try:
+            s.connect((self.ip, self.port))
+        except:
+            s.close()
+            print "Couldn't connect to server.\n"
+            return
+        s.send(json.dumps({ "ver": self.ver, "cmd": "update", "type": self.type }))
+        try:
+            response = s.recv(1024)
+        except socket.timeout:
+            print "Server seems to be offline.\n"
+            s.close()
+            return
+        s.close()
+        if response[0] == "0":
+            print "Your %s is running the latest version.\n" % self.type
+            return
+        
+        if response[0] == "1":
+            print "A new version is available. It can be downloaded at:\nhttps://raw.github.com/Max00355/BlooCoin/master/miner.py\n"
+            return
+
+        if response[0] == "2":
+            print response[1:] + "\n"
+            return
+        return
 
 if __name__ == "__main__":
     if len(sys.argv) != 3:
         print "Usage: python miner.py <threads> <bloocoin address>"
+        raw_input("Press enter to exit\r\n")
         exit()
     b = BlooMiner()
     for x in range(int(sys.argv[1])):
